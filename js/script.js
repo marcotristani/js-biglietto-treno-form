3033ff785aa736fa78dfe8e504ecfa7fdb258abe
marcotristani <marco.tristani@hotmail.it>
2025-12-11 13:31:26 +0100

    flat: scritta logica programma

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//FASE DI PREPARAZIONE
+//prendere collegamenti input dall'html
+//prendere  collegamento bottone dall'html
+
+//FASE DI ELABORAZIONE
+//Prendere da input pagina numero chilometri da percorrere
+//prendere da input pagina età passeggero
+
+// calcolare il prezzo del biglietto = numero kilometri * 0,21 euro
+
+//se età passeggero è minorenne(<18) allora
+     // calcolare sconto da applicare (prezzo biglietto * 0.2)(20% di sconto)
+     // prezzo biglietto ridotto = prezzo biglietto intero - sconto
+                       //OPPURE
+     // prezzo biglietto ridotto = prezzo biglietto *0.8
+
+//se età passeggero è over65(>65) allora
+     // calcolare sconto da applicare (prezzo biglietto * 0.4)(40% di sconto)
+     // prezzo biglietto ridotto = prezzo biglietto intero - sconto
+                       //OPPURE
+     // prezzo biglietto ridotto = prezzo biglietto *0.6
+
+//FASE DI OUTPUT
+//Quando viene fatto click sul bottone stampo il prezzo finale del biglietto con un console.log
